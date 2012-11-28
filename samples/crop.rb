55335967f1882586d503ca6521bb3e1b0fe4d0fe
Haifeng Cao <caohaifeng@gmail.com>
2012-11-28 16:14:04 -0500

    fix format

--- samples/crop.rb
+++ samples/crop.rb
@@ -4,7 +4,7 @@ require 'img_scalr_voodoo'
 image_file = 'image_hires.jpg'
 
 ImgScalrVoodoo.with_image(image_file) do |img|
-  img.with_crop(800, 400, 1200, 1000) do
-    |img2| img2.save("c_#{image_file}")
+  img.with_crop(800, 400, 1200, 1000) do |img2|
+    img2.save("c_#{image_file}")
   end
 end

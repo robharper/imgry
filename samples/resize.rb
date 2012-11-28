cfc24929cdfcda8d14254a633af26d6a11b684ea
Haifeng Cao <caohaifeng@gmail.com>
2012-11-28 16:23:52 -0500

    changed sample: resize

--- samples/resize.rb
+++ samples/resize.rb
@@ -5,5 +5,7 @@ image_file = 'image_hires.jpg'
 
 ImgScalrVoodoo.with_image(image_file) do |img|
   img.resize(500, 500).save("l_#{image_file}")
-  img.resize(150, 150).save("s_#{image_file}")
+  img.resize(150, 150) do |img2|
+    img2.save("s_#{image_file}")
+  end
 end

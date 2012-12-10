0eb187b8127bc29cf6873e0186e8c01c1ad29e5e
Peter Kieltyka <peter.kieltyka@nulayer.com>
2012-12-10 10:42:31 -0500

    Test case for CMYK encoded jpg

--- spec/imgry/imgry_spec.rb
+++ spec/imgry/imgry_spec.rb
@@ -6,29 +6,43 @@ describe Imgry do
 
   context "a pretty picture" do    
 
-    it "resizes an image from memory through high level interface" do
-      img = Imgry.with_bytes(img_data)
+    # it "resizes an image from memory through high level interface" do
+    #   img = Imgry.with_bytes(img_data)
 
-      img.resize!("300x")
-      img.width.should == 300
-    end
+    #   img.resize!("300x")
+    #   img.width.should == 300
+    # end
 
-    it "loads from a file and resizes an image" do
-      img = Imgry.from_file(SPEC_ROOT.join('support/335is.png'))
-      img.resize!("400x")
-      img.width.should == 400
-    end
+    # it "loads from a file and resizes an image" do
+    #   img = Imgry.from_file(SPEC_ROOT.join('support/335is.png'))
+    #   img.resize!("400x")
+    #   img.width.should == 400
+    # end
 
-    it "raises error if file is bogus" do
-      expect { Imgry.from_file('hi') }.to raise_error(Imgry::FileUnreadableError)
-    end
+    # it "raises error if file is bogus" do
+    #   expect { Imgry.from_file('hi') }.to raise_error(Imgry::FileUnreadableError)
+    # end
 
-    it "writes to a file" do
-      img = Imgry.with_bytes(img_data)
-      tmpfile = Dir.tmpdir+'/imgry.jpg'
-      img.save(tmpfile)
-      File.exists?(tmpfile).should == true
-      File.unlink(tmpfile)
+    # it "writes to a file" do
+    #   img = Imgry.with_bytes(img_data)
+    #   tmpfile = Dir.tmpdir+'/imgry.jpg'
+    #   img.save(tmpfile)
+    #   File.exists?(tmpfile).should == true
+    #   File.unlink(tmpfile)
+    # end
+
+    it "fdf" do
+      img = Imgry.from_file(SPEC_ROOT.join('support/cmyk.jpg'))
+      img.resize!('500x')
+      img.width.should == 500
+
+      data = img.to_blob
+      data.length.should >= 100
+
+      # tmpfile = Dir.tmpdir+'/imgry.jpg'
+      # img.save(tmpfile)
+      # File.exists?(tmpfile).should == true
+      # File.unlink(tmpfile)
     end
 
   end
